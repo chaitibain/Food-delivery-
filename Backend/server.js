58a28e9c31bec691b1f82b743fef4e0c923b7341
Chaiti Bain <chaitibain1@gmail.com>
2026-01-11 17:01:57 +0530

    server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import 'dotenv/config'
 import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 
 
 // app config
@@ -21,9 +22,9 @@ connectDB();
 //api endpoints
 app.use("/api/food", foodRouter)
 app.use("/images", express.static("uploads"));
-app.use("/api/user",userRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/order",ordersRouter)
+app.use("/api/user", userRouter)
+app.use("/api/cart", cartRouter)
+app.use("/api/order", orderRouter)
 
 
 app.get("/", (req, res) => {
@@ -33,3 +34,12 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`Server Started on http://localhost:${port}`)
 })
+
+
+// mongodb+srv://FoodDeliveryApp:<db_password>@cluster0.p3r48.mongodb.net/?
+
+//Find the PID (netstat -ano | findstr :4000)
+
+//Kill the process (taskkill /PID <PID> /F)
+
+//Restart the server (node server.js) or (npm run server)
